f4d5cc365c87bb76f591853e61f6a676ecb80095
MalikHw <help.malicorporation@gmail.com>
2026-04-28 11:50:41 +0100

    work

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,30 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/MenuLayer.hpp>
-#define WIN32_LEAN_AND_MEAN
-#include <windows.h>
+#include <Geode/modify/CCEGLView.hpp>
+#include <Geode/modify/CCApplication.hpp>
+#include <Geode/modify/PlayLayer.hpp>
 
 using namespace geode::prelude;
 
-LONG_PTR oWindowProc;
-bool newWindowProcSet = false;
-
-LRESULT CALLBACK nWindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
-    switch (msg) {
-	case WM_SIZE:
-	case WM_MOVE:
-    case WM_KILLFOCUS:
-        if (PlayLayer* playLayer = PlayLayer::get()) {
+void pauseGame() {
+    if (auto playLayer = PlayLayer::get()) {
+        if (!playLayer->m_isPaused) {
             playLayer->pauseGame(true);
         }
-        break;
     }
-
-    return CallWindowProc((WNDPROC)oWindowProc, hwnd, msg, wparam, lparam);
 }
 
-HWND getWindowHandle() {
-    return WindowFromDC(wglGetCurrentDC());
-}
+class $modify(CCEGLView) {
+    void windowFocusChanged(bool focused) {
+        CCEGLView::windowFocusChanged(focused);
+        if (!focused) {
+            pauseGame();
+        }
+    }
+};
 
-class $modify(MyMenuLayer, MenuLayer) {
-	bool init() {
-		if (!MenuLayer::init()) {
-			return false;
-		}
-
-		if (!newWindowProcSet) {
-			oWindowProc = SetWindowLongPtrA(getWindowHandle(), -4, (LONG_PTR)nWindowProc);
-    		newWindowProcSet = true;
-		}
-
-		return true;
-	}
+class $modify(CCApplication) {
+    void applicationDidEnterBackground() {
+        CCApplication::applicationDidEnterBackground();
+        pauseGame();
+    }
 };
